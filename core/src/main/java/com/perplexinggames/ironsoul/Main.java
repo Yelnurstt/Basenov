671334375e6abfa33089724e0ec57d5b9ab9e28e
EAOErmak <zrmek.aydarbekov@gmail.com>
2026-05-24 18:40:16 +0500

    refactor(Main): restore compatibility entry point for legacy package launchers

--- core/src/main/java/com/perplexinggames/ironsoul/Main.java
+++ core/src/main/java/com/perplexinggames/ironsoul/Main.java
@@ -1,12 +1,7 @@
 package com.perplexinggames.ironsoul;
 
-import com.badlogic.gdx.Game;
-import com.perplexinggames.ironsoul.screens.LevelEditorDemoScreen;
-
-/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
-public class Main extends Game {
-    @Override
-    public void create() {
-        setScreen(new LevelEditorDemoScreen());
-    }
+/**
+ * Compatibility entry point kept for launchers still importing the legacy package.
+ */
+public class Main extends com.perplexinggames.ironsoul.core.Main {
 }
