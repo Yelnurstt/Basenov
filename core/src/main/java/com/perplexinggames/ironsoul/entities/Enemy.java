81e95ce0994bb5d1a01c527a7ff8715caf6a76bf
sikymbaibekarysbolatbekuly <bekarys.sikymbai@narxoz.kz>
2026-05-24 22:14:29 +0500

    Add basic enemy entity architecture

--- core/src/main/java/com/perplexinggames/ironsoul/entities/Enemy.java
+++ core/src/main/java/com/perplexinggames/ironsoul/entities/Enemy.java
@@ -6,39 +6,36 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Rectangle;
 
-public class Enemy {
+public class Enemy extends GameEntity {
+
     private final Rectangle bounds;
     private final Texture texture;
 
-    private float health = 50f;
     private final float damage = 10f;
 
     public Enemy(float x, float y, float width, float height) {
+        super(x, y, 50f);
+
         bounds = new Rectangle(x, y, width, height);
 
         Pixmap pixmap = new Pixmap((int) width, (int) height, Pixmap.Format.RGBA8888);
         pixmap.setColor(Color.RED);
         pixmap.fill();
+
         texture = new Texture(pixmap);
         pixmap.dispose();
     }
 
+    @Override
     public void update(float delta) {
+        bounds.setPosition(x, y);
     }
 
+    @Override
     public void render(SpriteBatch batch) {
         if (!isDead()) {
             batch.draw(texture, bounds.x, bounds.y, bounds.width, bounds.height);
         }
-    }
-
-    public void takeDamage(float amount) {
-        health -= amount;
-        if (health < 0f) health = 0f;
-    }
-
-    public boolean isDead() {
-        return health <= 0f;
     }
 
     public Rectangle getBounds() {
@@ -47,10 +44,6 @@ public class Enemy {
 
     public float getDamage() {
         return damage;
-    }
-
-    public float getHealth() {
-        return health;
     }
 
     public void dispose() {
